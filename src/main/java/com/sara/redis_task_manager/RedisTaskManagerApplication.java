9fb7ef63a9707917c13dc1ee9596e558650448ad
Sara <82754823+SaraHadou@users.noreply.github.com>
2025-06-28 22:16:35 +0300

    initialize redis task manager project

---
+++ src/main/java/com/sara/redis_task_manager/RedisTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sara.redis_task_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisTaskManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sara/redis_task_manager/RedisTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sara.redis_task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
