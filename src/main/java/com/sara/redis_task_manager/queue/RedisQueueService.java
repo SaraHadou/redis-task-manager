66a7c05974415494dc2455c35b30754b06bd29ed
Sara <82754823+SaraHadou@users.noreply.github.com>
2025-06-29 07:20:18 +0300

    add RedisQueueService

---
+++ src/main/java/com/sara/redis_task_manager/queue/RedisQueueService.java
@@ -0,0 +1,24 @@
+package com.sara.redis_task_manager.queue;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisQueueService {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public RedisQueueService(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void pushToQueue(String queueName, String value) {
+        redisTemplate.opsForList().leftPush(queueName, value);
+    }
+
+    public String popFromQueue(String queueName) {
+        return redisTemplate.opsForList().rightPop(queueName);
+    }
+
+}
